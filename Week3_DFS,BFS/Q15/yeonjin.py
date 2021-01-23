d81f830b56208affa38985f736138684cc4f921f
yeunj <sayeunjin@gmail.com>
2021-01-23 18:38:27 +0900

    solve

---
+++ Week3_DFS,BFS/Q15/yeonjin.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+
+def bfs(graph, start, visited):
+    visited[start] = True
+    queue = deque([(start, 0)])
+    cnt = 0
+    ans = []
+    while queue:
+        v = queue.popleft()
+        cnt = v[1] + 1
+        for i in graph[v[0]]:
+            if not visited[i]:
+                visited[i] = True
+                # cnt 초에 i 노드 도착
+                queue.append((i, cnt))
+                if cnt == K:
+                    ans.append(i)
+    return ans
+
+
+N, M, K, X = map(int, input().split())
+graph = [[] for _ in range(N + 1)]
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+
+visited = [False] * (N + 1)
+ans = bfs(graph, X, visited)
+ans.sort()
+if len(ans) > 0:
+    for i in ans:
+        print(i)
+else:
+    print(-1)
